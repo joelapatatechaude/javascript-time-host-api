e69535b0950e090e41ad1ae86942e3fa1b089b53
Christophe Schmitz <cschmitz@redhat.com>
2024-06-05 18:01:02 +1000

    update

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-const API_ENDPOINT = 'http://java-time-host-api:8080';
-
-
 document.getElementById('fetchButton').addEventListener('click', () => {
+    const API_ENDPOINT = document.getElementById('apiEndpoint').value;
     fetch(`${API_ENDPOINT}/time`)
         .then(response => response.json())
         .then(data => {
@@ -17,14 +15,15 @@ document.getElementById('fetchButton').addEventListener('click', () => {
 });
 
 document.getElementById('fetchHistoryButton').addEventListener('click', () => {
+    const API_ENDPOINT = document.getElementById('apiEndpoint').value;
     fetch(`${API_ENDPOINT}/history`)
         .then(response => response.json())
         .then(data => {
             let historyHTML = '<ul>';
-            data.slice(0, 30).forEach(item => {
+            data.slice(0, 100).forEach(item => {
                 const dateTime = new Date(item.dateTime);
                 const formattedTime = dateTime.toLocaleTimeString();
-                historyHTML += `<li>Time: ${formattedTime}, Host: ${item.hostname}</li>`;
+                historyHTML += `<li>Time: ${formattedTime}, Hostname: ${item.hostname}</li>`;
             });
             historyHTML += '</ul>';
             document.getElementById('historyResult').innerHTML = historyHTML;
